65285dcdb48b688a5365f954b21bd0a6e439f329
tatianaanosova <anosova@hypeauditor.com>
2024-07-08 14:48:54 +0200

    Initial commit

---
+++ src/app/(pages)/estimation/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+	return (
+		<main>
+			<h1>Estimateion Page</h1>
+		</main>
+	);
+}
---
+++ src/app/layout.tsx
@@ -0,0 +1,25 @@
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
+import "./globals.css";
+
+const montserrat = Montserrat({
+  subsets: ["latin"],
+  display: 'swap',
+});
+
+export const metadata: Metadata = {
+  title: "Aivus Web",
+  description: "Aivus Web",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={montserrat.className}>{children}</body>
+    </html>
+  );
+}
