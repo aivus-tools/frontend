c2d66c50dd84763e2da1d3f670988d9c62370329
Dmitrii Emelianov <eme.di@ya.ru>
2025-02-07 16:39:11 +0100

    Remove pino logger

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -1,11 +1,3 @@
-import pino from 'pino';
-
-const logger = pino({
-	level: 'info',
-	transport: {
-		target: 'pino-pretty',
-		options: { colorize: true },
-	},
-});
+const logger = console;
 
 export default logger;
