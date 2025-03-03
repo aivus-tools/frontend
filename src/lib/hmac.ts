7daf61c3afa45048924b9649469c2b8b85574d3e
Dmitrii Emelianov <eme.di@ya.ru>
2025-03-03 15:34:08 +0100

    Log signature

--- src/lib/hmac.ts
+++ src/lib/hmac.ts
@@ -8,7 +8,10 @@ export async function createHmacSHA256(message: string) {
 	const cryptoKey = await crypto.subtle.importKey('raw', keyData, { name: 'HMAC', hash: 'SHA-256' }, false, ['sign']);
 	const signature = await crypto.subtle.sign('HMAC', cryptoKey, msgData);
 	// Преобразуем в hex-строку
-	return Array.from(new Uint8Array(signature))
+	const string = Array.from(new Uint8Array(signature))
 		.map((b) => b.toString(16).padStart(2, '0'))
 		.join('');
+
+	console.log('string', string);
+	return string;
 }
