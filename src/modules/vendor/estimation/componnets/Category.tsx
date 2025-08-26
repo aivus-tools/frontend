651f0a88473dd71ec84513ca787fe2a8e83868cf
Evgeniya Osmakova <evgeniya.osmakova@gmail.com>
2025-08-26 16:53:48 +0200

    rename

--- src/modules/vendor/estimation/componnets/Category.tsx
+++ src/modules/vendor/estimation/componnets/Category.tsx
@@ -22,7 +22,7 @@ export function Category({ category }: { category: TypeCategory }) {
   const subCategories = useAppSelector(
     useCallback((state: RootState) => selectSubcategoryById(state, category.id), [category.id])
   );
-  const offer = useAppSelector(
+  const offers = useAppSelector(
     useCallback((state: RootState) => selectOffersByCategoryId(state, category.id), [category.id])
   );
 
@@ -38,7 +38,7 @@ export function Category({ category }: { category: TypeCategory }) {
           {subCategories?.map((subCategory) => (
             <SubCategory key={`${key}${subCategory.id}`} subCategory={subCategory} />
           ))}
-          <Entries data={offer} />
+          <Entries data={offers} />
           <Total text={category.name} value={`$ ${total}`} clientValue={`$ ${clientTotal}`} categoryId={category.id} />
         </>
       )}
